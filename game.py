95d9ebb19145103aa719c7e21d1ab08df4d2e442
nekonnect <twyxavier@gmail.com>
2024-09-26 21:55:39 +0800

    Update Player Class
    Added Basic Collision detection for Enemy in Player class. and HP reduction
    
    TO DO
    Player Class
    Attack Function
    
    Enemy Class

--- game.py
+++ game.py
@@ -14,14 +14,33 @@ class Game:
     def game_loop(self):
         player = Player()
 
+        
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False        
 
+            pos = pygame.mouse.get_pos()
+            
+            #Collison Testing Shift to enenmy Class
+            enemy_rect = pygame.Rect(0, 0, 25, 25)
+            enemy_rect.center = pos
+            """if player.color == "YELLOW" and player.tick >= 4:
+                player.color = "GREEN"
+            else:
+                player.color = "GREEN"""
+            
+            #Testing Collision for Enemy Can be removed.
+            """if player.rect.colliderect(enemy_rect):
+               player.color = "RED"
+               player.hp -=1
+               print(player.hp)"""
+
+            
             #Draw Sprites to game window and updates.
             self.screen.fill((0,0,0))
-            player.playerUpdate(self.screen)
+            pygame.draw.rect(self.screen, "BLUE" , enemy_rect)    
+            player.playerUpdate(self.screen,enemy_rect)     #Change to a list containing enemy
             self.display.blit(self.screen, (0,0))
             pygame.display.flip()
             self.clock.tick(60) 
--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player():
         self.hp = self.max_hp
         self.dead = False
         self.score = 0
+        self.tick = 0
     
     #check player status
     def player_alive(self):
@@ -17,6 +18,10 @@ class Player():
             self.color = (255,0,0)
             self.dead = True
             #Player Dead
+        elif self.tick < 16:
+            self.color = "YELLOW"
+        else:
+            self.color = "GREEN"
 
     #Handle Player input
     def input(self):
@@ -31,11 +36,21 @@ class Player():
             if key[pygame.K_d]:
                 self.rect.move_ip(3,0)
 
+    def checkCollision(self,enemies):
+        #for enemy in enemies:
+            if self.rect.colliderect(enemies) and self.tick >= 4:
+                self.color = "YELLOW"
+                self.hp -=1
+                self.tick = 0
+                print(self.hp)
+
     #Draw player on to surface(Game screen)
     def draw(self,surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
-    def playerUpdate(self,surface):
+    def playerUpdate(self,surface,enemies):
         self.player_alive()
+        self.checkCollision(enemies)
         self.input()
         self.draw(surface)
+        self.tick +=1
